3aac33119f90961f58593f515ecff5986e0e1e05
Eesa-Khalil <Eesa-Khalil@users.noreply.github.com>
2020-05-15 00:35:34 +0100

    django_messages added

--- qmusocialnetwork/settings.py
+++ qmusocialnetwork/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_auth',
     'qmeet.apps.QmeetConfig',
+    'django_messages'
 ]
 
 MIDDLEWARE = [
--- qmusocialnetwork/urls.py
+++ qmusocialnetwork/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('qmeet/', include('django.contrib.auth.urls')),
+    path('messages/', include('django_messages.urls')),
     path('qmeet/', include('qmeet.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
