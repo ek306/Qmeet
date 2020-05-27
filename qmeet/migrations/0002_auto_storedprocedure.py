eedf22fd419067434558a53e8abee8bc4f125c47
Khalil Ahmed <ahmed.khalil@ntlworld.com>
2020-05-27 20:19:44 +0100

    changed unread messages

--- qmeet/migrations/0002_auto_storedprocedure.py
+++ qmeet/migrations/0002_auto_storedprocedure.py
@@ -167,7 +167,8 @@ class Migration(migrations.Migration):
                 IN UserID int
             )
             BEGIN
-                select count(*) as 'un_read', subject, sender_id from django_messages_message
+                select count(*) as 'un_read'
+                from django_messages_message
                 where read_at is null and recipient_id=UserID;
             END$$
             DELIMITER ;
